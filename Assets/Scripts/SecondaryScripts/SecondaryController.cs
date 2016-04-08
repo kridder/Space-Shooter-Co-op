95ae91c12bca9fe92a77f6d18b24942832c697b8
Kevin de Ridder <kridder@hotmail.com>
2016-04-08 02:38:13 -0700

    Optimized function spawnPowerups

--- Assets/Scripts/SecondaryScripts/SecondaryController.cs
+++ Assets/Scripts/SecondaryScripts/SecondaryController.cs
@@ -23,6 +23,18 @@ public class SecondaryController : MonoBehaviour {
 			StartCoroutine (spawnPowerups ());
 			Debug.Log("p key was pressed");
 		}
+
+		if (Input.GetKeyDown (KeyCode.O)) 
+		{
+			foreach (GameObject i in grid[0])
+			{
+				if (i != null)
+				{
+					Debug.Log (i.transform.position + " " + i.tag);
+				}
+			}
+			Debug.Log("o key was pressed");
+		}
 	}
 
 	// Powerup Spawner
@@ -30,33 +42,34 @@ public class SecondaryController : MonoBehaviour {
 	{
 		for (int i = 0; i < grid.Length; i++)
 		{
+			bool spawnNew = false;
 			for (int ii = 0; ii < grid[i].Length; ii++)
 			{
-				if (grid[i][ii] == null)
+				if (!spawnNew && grid[i][ii] == null)
 				{
-					bool spawnNew = true;
-					for (int iii = ii + 1; iii < grid[i].Length; iii++)
+					spawnNew = true;
+					for (int iii = ii + 1; iii < grid [i].Length; iii++)
 					{
 						if (grid[i][iii] != null)
 						{
-							grid[i][ii] = grid[i][iii];
+							grid[i][ii] = grid [i] [iii];
 							grid[i][ii].GetComponent<MatchObjectController> ().yPos = ii;
 							grid[i][iii] = null;
 							spawnNew = false;
 							break;
 						}
 					}
+				}
 
-					if (spawnNew)
-					{
-						GameObject powerup = powerups [Random.Range (0, powerups.Length)];
-						Vector3 spawnPosition = new Vector3 (i, 12, 0);
-						Quaternion spawnRotation = Quaternion.identity;
-						GameObject spawned = Instantiate (powerup, spawnPosition, spawnRotation) as GameObject;
-						spawned.GetComponent<MatchObjectController> ().yPos = ii;
-						grid[i][ii] = spawned;
-						yield return new WaitForSeconds (0.05f);
-					}
+				if (spawnNew)
+				{
+					GameObject powerup = powerups [Random.Range (0, powerups.Length)];
+					Vector3 spawnPosition = new Vector3 (i, 12, 0);
+					Quaternion spawnRotation = Quaternion.identity;
+					GameObject spawned = Instantiate (powerup, spawnPosition, spawnRotation) as GameObject;
+					spawned.GetComponent<MatchObjectController> ().yPos = ii;
+					grid[i][ii] = spawned;
+					yield return new WaitForSeconds (0.05f);
 				}
 			}
 		}
