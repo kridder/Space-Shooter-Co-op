7d5841d62793ffab519ccaeb04993f86b9838785
Kevin de Ridder <kridder@hotmail.com>
2016-04-10 02:44:37 -0700

    Added functions for detecting horizontal and vertical matches, and deleting those matched out of the grid.

--- Assets/Scripts/SecondaryScripts/SecondaryController.cs
+++ Assets/Scripts/SecondaryScripts/SecondaryController.cs
@@ -1,30 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SecondaryController : MonoBehaviour {
 
+	public const int gridX = 8;
+	public const int gridY = 12;
+
 	public GameObject[] powerups;
 	//private GameObject[,] grid = new GameObject[8,12];
-	private GameObject[][] grid = new GameObject[8][];
+	private GameObject[][] grid = new GameObject[gridX][];
+	private List<GameObject> Match = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
 		for (int i = 0; i < grid.Length; i++)
 		{
-			grid[i] = new GameObject[12];
+			grid[i] = new GameObject[gridY];
 		}
 		StartCoroutine (InitSpawnPowerups ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.P)) 
+		if (Input.GetKeyDown (KeyCode.X)) 
 		{
 			StartCoroutine (spawnPowerups ());
-			Debug.Log("p key was pressed");
 		}
 
-		if (Input.GetKeyDown (KeyCode.O)) 
+		// Print list of logged powerups
+		if (Input.GetKeyDown (KeyCode.Z)) 
 		{
 			foreach (GameObject i in grid[0])
 			{
@@ -33,26 +38,61 @@ public class SecondaryController : MonoBehaviour {
 					Debug.Log (i.transform.position + " " + i.tag);
 				}
 			}
-			Debug.Log("o key was pressed");
 		}
+
+		if (Input.GetKeyDown (KeyCode.C)) 
+		{
+			checkMatch ();
+		}
+
+		if (Input.GetKeyDown (KeyCode.V)) 
+		{
+			deleteMatches();
+		}
+	}
+		
+	// Spawn initial playfield
+	IEnumerator InitSpawnPowerups ()
+	{
+		for (int i = 0; i < gridX; i++)
+		{
+			for (int ii = 0; ii < gridY; ii++)
+			{
+				GameObject powerup = powerups [Random.Range (0, powerups.Length)];
+				Vector3 spawnPosition = new Vector3 (i, 12, 0);
+				Quaternion spawnRotation = Quaternion.identity;
+				GameObject spawned = Instantiate (powerup, spawnPosition, spawnRotation) as GameObject;
+				spawned.GetComponent<MatchObjectController> ().yPos = ii;
+				grid[i][ii] = spawned;
+				yield return new WaitForSeconds (0.05f);
+			}
+		}
+
+		/*
+		foreach (GameObject i in grid[0])
+		{
+			Debug.Log (i.tag);
+		}
+		Debug.Log (grid[3][9].tag);
+		*/
 	}
 
 	// Powerup Spawner
 	IEnumerator spawnPowerups ()
 	{
-		for (int i = 0; i < grid.Length; i++)
+		for (int i = 0; i < gridX; i++)
 		{
 			bool spawnNew = false;
-			for (int ii = 0; ii < grid[i].Length; ii++)
+			for (int ii = 0; ii < gridY; ii++)
 			{
 				if (!spawnNew && grid[i][ii] == null)
 				{
 					spawnNew = true;
-					for (int iii = ii + 1; iii < grid [i].Length; iii++)
+					for (int iii = ii + 1; iii < gridY; iii++)
 					{
 						if (grid[i][iii] != null)
 						{
-							grid[i][ii] = grid [i] [iii];
+							grid[i][ii] = grid[i][iii];
 							grid[i][ii].GetComponent<MatchObjectController> ().yPos = ii;
 							grid[i][iii] = null;
 							spawnNew = false;
@@ -74,34 +114,94 @@ public class SecondaryController : MonoBehaviour {
 			}
 		}
 
-		foreach (GameObject i in grid[0])
-		{
-			Debug.Log (i.tag);
-		}
-		Debug.Log (grid [3] [9].tag);
+//		foreach (GameObject i in grid[0])
+//		{
+//			Debug.Log (i.tag);
+//		}
+//		Debug.Log ("object x.3 y.9"+grid[3][9].tag);
 	}
 
-	// Spawn initial playfield
-	IEnumerator InitSpawnPowerups ()
+	// Check for matches
+	void checkMatch()
 	{
-		for (int i = 0; i < grid.Length; i++)
+		//Debug.Log (grid[0][0].tag);
+		// Check for horizontal matches
+		for (int i = 0; i < gridY; i++)
 		{
-			for (int ii = 0; ii < grid[i].Length; ii++)
+			for (int ii = 0; ii < gridX; ii++)
 			{
-				GameObject powerup = powerups [Random.Range (0, powerups.Length)];
-				Vector3 spawnPosition = new Vector3 (i, 12, 0);
-				Quaternion spawnRotation = Quaternion.identity;
-				GameObject spawned = Instantiate (powerup, spawnPosition, spawnRotation) as GameObject;
-				spawned.GetComponent<MatchObjectController> ().yPos = ii;
-				grid[i][ii] = spawned;
-				yield return new WaitForSeconds (0.05f);
+				//Debug.Log("grid["+i+"]["+ii+"]");
+				//GameObject[] tempMatch = new GameObject[];
+				List<GameObject> tempMatch = new List<GameObject>();
+				tempMatch.Add (grid[ii][i]);
+				int iii;
+				for (iii = ii + 1; iii < gridX; iii++)
+				{
+					if (grid[ii][i].tag == grid[iii][i].tag)
+					{
+						tempMatch.Add (grid[iii][i]);
+					} else {
+						break;
+					}
+				}
+
+				if (tempMatch.Count >= 3)
+				{
+					// Debug.Log("Match: " + tempMatch);
+					Debug.Log("Horizontal match on object y."+i+" x."+ii+"; counted: "+tempMatch.Count+"; of type: "+grid[ii][i].tag);
+					foreach (GameObject x in tempMatch)
+					{
+						Match.Add(x);
+					}
+				}
+
+				ii = iii - 1;
 			}
 		}
 
-		foreach (GameObject i in grid[0])
+		// Check for vertical matches
+		for (int i = 0; i < gridX; i++)
 		{
-			Debug.Log (i.tag);
+			for (int ii = 0; ii < gridY; ii++)
+			{
+				//Debug.Log("grid["+i+"]["+ii+"]");
+				//GameObject[] tempMatch = new GameObject[];
+				List<GameObject> tempMatch = new List<GameObject>();
+				tempMatch.Add (grid[i][ii]);
+				int iii;
+				for (iii = ii + 1; iii < gridY; iii++)
+				{
+					if (grid[i][ii].tag == grid[i][iii].tag)
+					{
+						tempMatch.Add (grid[i][iii]);
+					} else {
+						break;
+					}
+				}
+
+				if (tempMatch.Count >= 3)
+				{
+					// Debug.Log("Match: " + tempMatch);
+					Debug.Log("Vertical match on object x."+i+" y."+ii+"; counted: "+tempMatch.Count+"; of type: "+grid[i][ii].tag);
+					foreach (GameObject x in tempMatch)
+					{
+						Match.Add(x);
+					}
+				}
+
+				ii = iii - 1;
+			}
 		}
-		Debug.Log (grid [3] [9].tag);
+	}
+
+	// Delete matches
+	void deleteMatches()
+	{
+		foreach (GameObject i in Match)
+		{
+			Destroy(i);
+		}
+		Match.Clear();
+		Match.TrimExcess();
 	}
 }
